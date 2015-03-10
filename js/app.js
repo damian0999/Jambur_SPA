ebfd827cda0a381626fb10e48e4082f58c93e8c5
Damian <labasdamian@gmail.com>
2015-03-10 18:00:31 +0100

    First page, with scroll menu

--- js/app.js
+++ js/app.js
@@ -1,32 +1,36 @@
 $(document).ready(function(){
-	$('.navbar').animate({
-    opacity: .7
-  }, 100);
 
+	$(window).scroll(function(e) {
+     currentposition = $(document).scrollTop();//aktualna pozycja względem początku          
+     bannerheight = $("#navigation").height();//wysokość elementu, po którym menu ma stać się białe
+      if(currentposition>bannerheight)
+       {         
+           $("#navigation").addClass("navbar_scroll");
+       }
+       else
+          $("#navigation").removeClass("navbar_scroll");
+  });
+  
   $('#arrow_header').hover(function(){
-    $('#img_header').attr('src', 'img/gory.jpg');
+    $('#img_header').attr('src', 'img/gory.jpg')
+    $('#navigation').animate({
+      opacity: 0
+    }, 1000);
   }, function(){
-    $('#img_header').attr('src', 'img/gory_szary.jpg')
+    $('#img_header').attr('src', 'img/gory_szary.jpg');
+      $('#navigation').animate({
+        opacity: .7
+      }, 1000);
   });
 
   $('#arrow_header').hover(function(){
   	$(this).stop().animate({
   		fontSize: '92px'
-  	}, 700)
+  	}, 700);
   }, function(){
   	$(this).stop().animate({
   		fontSize: '68px'
-  	}, 700)
-  });
-
-  $('arrow_header').hover(function(){
-  	$(this).animate({
-
-  	}, 1500);
-  }, function(){
-  	$(this).animate({
-
-  	}, 1500);
+  	}, 700);
   });
 
   $("#lightSlider").lightSlider({
