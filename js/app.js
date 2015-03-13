df91956cdc2c3d361c879604a477120a6119fdf1
Damian <labasdamian@gmail.com>
2015-03-13 23:49:23 +0100

    interactive nabar

--- js/app.js
+++ js/app.js
@@ -1,36 +1,45 @@
 $(document).ready(function(){
 
 	$(window).scroll(function(e) {
-     currentposition = $(document).scrollTop();//aktualna pozycja względem początku          
-     bannerheight = $("#navigation").height();//wysokość elementu, po którym menu ma stać się białe
-      if(currentposition>690)
-       {         
-        $("#navigation").addClass("navbar_scroll");
 
-        $('.page-scroll').stop().animate({
-          bottom: '28px'
-        }, 150);
+    var img_height = $('#myCarousel').height();
+    var navbar_height = $('#navigation').height();
+    var header_jambur = $('.carousel-caption').position().top;
 
-        $('.jambur_nav').stop().animate({
-          opacity: 1
-        }, 100);
-       }
-       else
-       {
-        $("#navigation").removeClass("navbar_scroll");
+    currentposition = $(document).scrollTop();      
+    if(currentposition>img_height - navbar_height){         
+      $("#navigation").addClass("navbar_scroll");
 
-        $('.page-scroll').stop().animate({
-          bottom: '0px'
-        }, 150);
+      $('.page-scroll').stop().animate({
+        bottom: '28px'
+      }, 150);
 
-        $('.jambur_nav').stop().animate({
-          opacity: 0
-        }, 100);
-       }
+    }
+
+    if(currentposition>header_jambur){
+      $('.jambur_nav').stop().animate({
+        opacity: 1
+      }, 100);
+    }
+
+
+    if(currentposition< img_height - navbar_height){
+      $("#navigation").removeClass("navbar_scroll");
+
+      $('.page-scroll').stop().animate({
+        bottom: '0px'
+      }, 150);
+    }
+
+    if(currentposition<header_jambur){
+      $('.jambur_nav').stop().animate({
+        opacity: 0
+      }, 100);
+    }
   });
   
   $('#arrow_header').hover(function(){
-    $('#img_header').attr('src', 'img/gory.jpg')
+    $('#img_header').attr('src', 'img/gory.jpg');
     $('#navigation').stop().animate({
       opacity: 0
     }, 1000);
