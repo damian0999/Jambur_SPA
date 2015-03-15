7577d077a03464d6c31d603ec616e4cbecf2991f
Damian <labasdamian@gmail.com>
2015-03-15 20:09:28 +0100

    simple animation, little change with navabr

--- js/app.js
+++ js/app.js
@@ -1,43 +1,5 @@
 $(document).ready(function(){
-
-	$(window).scroll(function(e) {
-
-    var img_height = $('#myCarousel').height();
-    var navbar_height = $('#navigation').height();
-    var header_jambur = $('.carousel-caption').position().top;
-
-    currentposition = $(document).scrollTop();      
-    if(currentposition>img_height - navbar_height){         
-      $("#navigation").addClass("navbar_scroll");
-
-      $('.page-scroll').stop().animate({
-        bottom: '28px'
-      }, 150);
-
-    }
-
-    if(currentposition>header_jambur){
-      $('.jambur_nav').stop().animate({
-        opacity: 1
-      }, 100);
-    }
-
-
-    if(currentposition< img_height - navbar_height){
-      $("#navigation").removeClass("navbar_scroll");
-
-      $('.page-scroll').stop().animate({
-        bottom: '0px'
-      }, 150);
-    }
-
-    if(currentposition<header_jambur){
-      $('.jambur_nav').stop().animate({
-        opacity: 0
-      }, 100);
-    }
-  });
-  
+ 
   $('#arrow_header').hover(function(){
     $('#img_header').attr('src', 'img/gory.jpg');
     $('#navigation').stop().animate({
