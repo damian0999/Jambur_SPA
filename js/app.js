cfd82139ebdbcbc9dde86dfc3b7b5bfed06f64a3
Damian <labasdamian@gmail.com>
2015-03-18 22:07:58 +0100

    new better code

--- js/app.js
+++ js/app.js
@@ -1,29 +1,19 @@
 $(document).ready(function(){
- 
-  $('#arrow_header').hover(function(){
-    $('#img_header').attr('src', 'img/gory.jpg');
-    $('#navigation').stop().animate({
-      opacity: 0
-    }, 1000);
-  }, function(){
-    $('#img_header').attr('src', 'img/gory_szary.jpg');
-      $('#navigation').stop().animate({
-        opacity: .7
-      }, 1000);
-  });
-
-  $('#arrow_header').hover(function(){
-  	$(this).stop().animate({
-  		fontSize: '92px'
-  	}, 700);
-  }, function(){
-  	$(this).stop().animate({
-  		fontSize: '68px'
-  	}, 700);
-  });
-
-  $('.carousel').carousel({
-    interval: 5000 
-  });
-
+	$('#arrow').hover(function(){
+		$('#img_header').attr('src', 'img/gory.jpg');
+		$('#arrow').stop().animate({
+			fontSize: '92px'
+		}, 700);
+		$('#navigation').stop().animate({
+			opacity: 0
+		}, 1000);
+	}, function(){
+		$('#img_header').attr('src', 'img/gory_szary.jpg');
+		$('#arrow').stop().animate({
+			fontSize: '68px'
+		}, 700);
+		$('#navigation').stop().animate({
+			opacity: .7
+		}, 1000);
+	});
 });
